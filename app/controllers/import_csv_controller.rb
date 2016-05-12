f8eb0d6af2cbd7800d09a3a2142025945e19d5f6
nicchi <nicchi.achan@gmail.com>
2016-05-13 00:08:40 +0900

    add unicorn

--- app/controllers/import_csv_controller.rb
+++ app/controllers/import_csv_controller.rb
@@ -1,4 +1,7 @@
 class ImportCsvController < ApplicationController
+  def index
+    render :json => {'msg': 'hello world'}
+  end
   def import
     # broker = params[:broker]
     trades = Trade.create_from_csv(params[:file])
---
+++ config/unicorn.rb
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+rails_root = File.expand_path('../../', __FILE__)
+ENV['BUNDLE_GEMFILE'] = rails_root + "/Gemfile"
+
+# ワーカーの数
+worker_processes 2
+
+# ソケット
+listen  "#{rails_root}/tmp/unicorn.sock"
+pid     "#{rails_root}/tmp/unicorn.pid"
+
+# ログ
+log = "#{rails_root}/log/unicorn.log"
+stderr_path File.expand_path('log/unicorn_stderr.log', rails_root)
+stdout_path File.expand_path('log/unicorn_stdout.log', rails_root)
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+before_fork do |server, worker|
+defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+
+old_pid = "#{ server.config[:pid] }.oldbin"
+unless old_pid == server.pid
+  begin
+   sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+   Process.kill :QUIT, File.read(old_pid).to_i
+   rescue Errno::ENOENT, Errno::ESRCH
+  end
+end
+end
+
+after_fork do |server, worker|
+    defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
