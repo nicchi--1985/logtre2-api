1e9b0b73e012d2fa232bfff4a79637910ce9bc39
nicchi <nicchi.achan@gmail.com>
2016-07-30 09:09:43 +0900

    refactor index calculator

--- app/models/concerns/index_calculator.rb
+++ app/models/concerns/index_calculator.rb
@@ -6,17 +6,19 @@ class IndexCalculator
     def self.new_trade_remaining_days_avg(trades)
         new_trades = trades.where(trade_type: TradeTypeEnum::BUY)
         remaining_days = new_trades.map {|t| t.sq_date - t.trade_datetime.to_date}
-        avg = remaining_days.inject(0.0){|sum, day| sum + day} / remaining_days.size
+        remaining_days.avg
     end
 
     def self.settlement_trade_remaining_days_avg(trades)
-        new_trades = trades.where(trade_type: TradeTypeEnum::SELL)
-        remaining_days = new_trades.map {|t| t.sq_date - t.trade_datetime.to_date}
-        avg = remaining_days.inject(0.0){|sum, day| sum + day} / remaining_days.size
+        settlement_trades = trades.where(trade_type: TradeTypeEnum::SELL)
+        remaining_days = settlement_trades.map {|t| t.sq_date - t.trade_datetime.to_date}
+        remaining_days.avg
     end
 
     def self.holding_days_avg(trades)
-
+        settlement_trades = trades.where(trade_type: TradeTypeEnum::SELL)
+        holding_days = settlement_trades.map {|t| t.trade_datetime.to_date - t.buy_date}
+        holding_days.avg
     end
 
     def self.max_gain_loss_amount(trades)
@@ -28,16 +30,12 @@ class IndexCalculator
     end
 
     def self.gain_loss_amount_avg(trades)
-        trade_count = trades.where(trade_type: TradeTypeEnum::SELL).count
-        total = trades.sum(:gain_loss_amount)
-        avg = total / trade_count
+        amounts = trades.where(trade_type: TradeTypeEnum::SELL).map{|t| t.gain_loss_amount}
+        amounts.avg
     end
 
     def self.gain_loss_amount_sd(trades)
         amounts = trades.where(trade_type: TradeTypeEnum::SELL).map{|t| t.gain_loss_amount}
-        count = amounts.size
-        avg = amounts.inject(0.0){|sum, amount| sum + amount } / count
-        var = amounts.reduce(0){|sum, amount| sum + (amount - avg) ** 2 } / (count - 1)
-        sd = Math.sqrt(var)
+        amounts.sd
     end
 end
